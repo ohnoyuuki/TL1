c0d6d524fc0c8257ddc76484fb3b15cda837d91d
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-06-14 23:00:27 +0900

    Update level_editor.py
    TL1_01_05

--- level_editor.py
+++ level_editor.py
@@ -15,6 +15,7 @@ bl_info = {
     "category": "Object"
 }
 
+
 # メニュー項目描画
 def draw_menu_manual(self, context):
     #self : 呼び出し元のクラスインスタンス。C++でいうthisポインタ
@@ -55,22 +56,18 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     def draw(self, context):
         
         #トップバーの「エディターメニュー」に項目（オペレータ）を追加
-        self.layout.operator("wm.url_open_preset", 
-            text="Manual", icon='HELP')
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, 
+            text=MYADDON_OT_stretch_vertex.bl_label)
+            
+        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, 
+            text=MYADDON_OT_create_ico_sphere.bl_label)
             
     #既存のメニューにサブメニューを追加
     def submenu(self, context):
         
         #ID指定でサブメニューを追加
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
-        
 
-        
-# Blenderブレンダーに登録するクラスリスト
-classes = (
-    TOPBAR_MT_my_menu,
-)
-    
 #オペレータ 頂点を伸ばす
 class MYADDON_OT_stretch_vertex(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_stretch_vertex"
@@ -86,6 +83,29 @@ class MYADDON_OT_stretch_vertex(bpy.types.Operator):
 
         #オペレータの命令終了を通知
         return {'FINISHED'} 
+    
+#オペレータ ICO球生成
+class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_create_object"
+    bl_label = "ICO球生成"
+    bl_description = "ICO球を生成します"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    # メニューを実行したときの呼ばれる関数
+    def execute(self, context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました。")
+
+        return {'FINISHED'}
+    
+
+
+# Blenderブレンダーに登録するクラスリスト
+classes = (
+    MYADDON_OT_stretch_vertex,
+    MYADDON_OT_create_ico_sphere,
+    TOPBAR_MT_my_menu,
+)
 
 # テスト実行用コード
 if __name__ == "__main__":
