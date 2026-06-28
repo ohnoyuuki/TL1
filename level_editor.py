4f0a8ee1aaa87944ec3e8ffcea6e2b8917343330
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-06-29 01:12:14 +0900

    Update level_editor.py
    TL1_01_08

--- level_editor.py
+++ level_editor.py
@@ -148,7 +148,7 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
             indent += "\t"
         
         #オブジェクト名書き込み
-        self.write_and_print(file, indent + object.type + " - " + object.name)
+        self.write_and_print(file, indent + object.type)
         trans, rot, scale = object.matrix_local.decompose()
         
         #回転を Quaternion から Euler（3軸での回転角）に変換
@@ -160,9 +160,13 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
         rot.z = math.degrees(rot.z)
         
         #トランスフォーム情報を表示
-        self.write_and_print(file, indent +  "Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
-        self.write_and_print(file, indent +  "Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z))
-        self.write_and_print(file, indent +  "Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+        self.write_and_print(file, indent +  "T(%f,%f,%f)" % (trans.x, trans.y, trans.z))
+        self.write_and_print(file, indent +  "R(%f,%f,%f)" % (rot.x, rot.y, rot.z))
+        self.write_and_print(file, indent +  "S(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+        #カスタムプロパティ'file_name'
+        if "file_name" in object:
+            self.write_and_print(file, indent + "N %s" % object['file_name'])
+        self.write_and_print(file, indent + "END")
         self.write_and_print(file, '')
 
         # 子ノードへ進む（深さが1上がる）
@@ -183,13 +187,54 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
 
         return {'FINISHED'}
 
+class OBJECT_PT_file_name(bpy.types.Panel):
+    """オブジェクトのファイルネームパネル"""
+    bl_idname = "OBJECT_PT_file_name"
+    bl_label = "FileName"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
+    # サブメニューの描画
+    def draw(self, context):
+        
+        # #パネルに項目を追加
+        # self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
+        # self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
+        # self.layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
+
+
+
+        if "file_name" in context.object:
+            #すでにプロパティがあれば、プロパティを表示
+            self.layout.prop(context.object, '["file_name"]', text=self.bl_label)
+        else:
+            #プロパティがなければ、プロパティ追加ボタンを表示
+            self.layout.operator(MYADDON_OT_add_filename.bl_idname)
+                
+
+class MYADDON_OT_add_filename(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_add_filename"
+    bl_label = "FileName 追加"
+    bl_description = "['file_name']カスタムプロパティを追加します"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+
+        #['file_name']カスタムプロパティを追加
+        context.object['file_name'] = ""
+
+        return {'FINISHED'}
+
 
 # Blenderブレンダーに登録するクラスリスト
 classes = (
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_create_ico_sphere,
     MYADDON_OT_export_scene,
+    MYADDON_OT_add_filename,
     TOPBAR_MT_my_menu,
+    OBJECT_PT_file_name,
 )
 
 # テスト実行用コード
