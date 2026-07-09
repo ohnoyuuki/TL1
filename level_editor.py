542950d13cf347d1809559c6406bb7357161eed6
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-07-09 22:57:48 +0900

    Update level_editor.py
    TL1_01_09

--- level_editor.py
+++ level_editor.py
@@ -1,6 +1,9 @@
 import bpy
 import math
 import bpy_extras
+import gpu
+import gpu_extras.batch
+import copy
 
 # ブレンダーに登録するアドオン情報
 bl_info = {
@@ -34,12 +37,17 @@ def register():
     
     #メニューに項目を追加
     bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    #3Dビューに描画関数を追加
+    DrawCollider.handle = bpy.types.SpaceView3D.draw_handler_add(DrawCollider.draw_collider, (), 'WINDOW', 'POST_VIEW')
     print("レべルエディタが有効化されました。")
     
 #Add-On無効化時コールバック
 def unregister():
     #メニューから項目を削除
     bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    #3Dビューから描画関数を削除
+    bpy.types.SpaceView3D.draw_handler_remove(DrawCollider.handle, 'WINDOW')
+    
     # Blenderからクラスを削除
     for cls in classes:
         bpy.utils.unregister_class(cls)
@@ -198,13 +206,7 @@ class OBJECT_PT_file_name(bpy.types.Panel):
     # サブメニューの描画
     def draw(self, context):
         
-        # #パネルに項目を追加
-        # self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
-        # self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
-        # self.layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
-
-
-
+        #パネルに項目を追加
         if "file_name" in context.object:
             #すでにプロパティがあれば、プロパティを表示
             self.layout.prop(context.object, '["file_name"]', text=self.bl_label)
@@ -226,6 +228,79 @@ class MYADDON_OT_add_filename(bpy.types.Operator):
 
         return {'FINISHED'}
 
+#コライダー描画
+class DrawCollider:
+
+    #描画ハンドル
+    handle = None
+
+    #3Dビューに登録する描画関数
+    def draw_collider():
+        #頂点データ
+        vertices = {"pos":[]}
+        #インデックスデータ
+        indices = []
+
+        #各頂点の、オブジェクト中心からのオフセット
+        offsets = [
+                    [-0.5,-0.5,-0.5], #左下前
+                    [+0.5,-0.5,-0.5],  #右下前
+                    [-0.5,+0.5,-0.5],  #左上前
+                    [+0.5,+0.5,-0.5],   #右上前
+                    [-0.5,-0.5,0.5],  #左下奥
+                    [+0.5,-0.5,0.5],   #右下奥
+                    [-0.5,+0.5,0.5],   #左上奥
+                    [+0.5,+0.5,0.5],    #右上奥
+        ]
+
+        #立方体のX,Y,Z方向サイズ
+        size = [2.0,2.0,2.0] 
+
+        #現在シーンのオブジェクトリストを走査
+        for object in bpy.context.scene.objects:
+            #追加前の頂点数
+            start = len(vertices["pos"])
+
+            #Boxの8頂点分回す
+            for offset in offsets:
+                #オブジェクトの中心座標をコピー
+
+                pos = copy.copy(object.location)
+                #中心点を基準に各頂点ごとにずらす
+                pos[0]+= offset[0]*size[0]
+                pos[1]+= offset[1]*size[1]
+                pos[2]+= offset[2]*size[2]
+                #頂点データリストに座標を追加
+                vertices["pos"].append(pos)
+
+                #前面を構成する辺の頂点インデックス
+                indices.append([start+0,start+1])
+                indices.append([start+2,start+3])
+                indices.append([start+0,start+2])
+                indices.append([start+1,start+3])
+                #裏面を構成する辺の頂点インデックス
+                indices.append([start+4,start+5])
+                indices.append([start+6,start+7])
+                indices.append([start+4,start+6])
+                indices.append([start+5,start+7])
+                #手前と奥を繋ぐ辺の頂点インデックス
+                indices.append([start+0,start+4])
+                indices.append([start+1,start+5])
+                indices.append([start+2,start+6])
+                indices.append([start+3,start+7])
+
+        #ビルド院のシェーダを取得
+        shader = gpu.shader.from_builtin('UNIFORM_COLOR')
+
+        #バッチを作成(引数：シェーダ、トポロジー、頂点データ、インデックスデータ)
+        batch = gpu_extras.batch.batch_for_shader(shader, "LINES", vertices, indices = indices)
+
+        #シェーダのパラメータ設定
+        color = [0.5, 1.0,1.0,1.0]
+        shader.bind()
+        shader.uniform_float("color", color)
+        #描画
+        batch.draw(shader)
 
 # Blenderブレンダーに登録するクラスリスト
 classes = (
